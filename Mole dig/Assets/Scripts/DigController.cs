17245f15bb4a05a29db1e8f93c2f187acea685c6
HemmingP <hemming_lol@hotmail.com>
2023-02-04 11:45:04 +0000

    Controller complete

--- Mole dig/Assets/Scripts/DigController.cs
+++ Mole dig/Assets/Scripts/DigController.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class DigController : MonoBehaviour
 {
 
     public float digSpeed = 5f;
-    
     public float angleSmoothTime = 0.1f;
-
     Rigidbody2D rigidbody2D => GetComponent<Rigidbody2D>();
+    private float movementX, movementY;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +20,8 @@ public class DigController : MonoBehaviour
     float zVelocity;
     // Update is called once per frame
     void FixedUpdate()
-    {
-        print(Input.GetAxis("Horizontal")+","+Input.GetAxis("Vertical"));
-        print(Input.GetAxisRaw("Horizontal")+","+Input.GetAxisRaw("Vertical"));
-        
-        Vector2 axisInputs = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+    {   
+        Vector2 axisInputs = new Vector2(movementX,movementY);
         if(axisInputs.magnitude > 1)
         {
             axisInputs.Normalize();
@@ -38,5 +35,12 @@ public class DigController : MonoBehaviour
             float zAxis = Mathf.SmoothDampAngle(transform.eulerAngles.z, Mathf.Atan2(axisInputs.y, axisInputs.x) * Mathf.Rad2Deg,ref zVelocity, angleSmoothTime);
             transform.eulerAngles = new Vector3(0, 0, zAxis);
         }
+    }
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 }
